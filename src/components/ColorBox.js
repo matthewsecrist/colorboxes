37fb373046c2e8e96b097f52060975b8190fbb9a
Matthew Secrist <matthew.secrist@outlook.com>
2018-12-30 15:52:36 -0600

    Move Colorbox into Components

---
+++ src/components/ColorBox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box, ColorName } from '../components'
+
+const ColorBox = ({ color, clicked, handleClick }) => {
+  return (
+    <Box style={{ background: color }} onClick={handleClick}>
+      <ColorName clicked={clicked}>{color}</ColorName>
+    </Box>
+  )
+}
+
+ColorBox.propTypes = {
+  color: PropTypes.string,
+  clicked: PropTypes.bool,
+  handleClick: PropTypes.func
+}
+
+export default ColorBox
