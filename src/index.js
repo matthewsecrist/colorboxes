32d8e583fe0b7f272dca631770961970bccfce43
Matthew Secrist <matthew.secrist@outlook.com>
2018-12-31 12:32:51 -0600

    Take store out of index.js and move to its own file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import App from './App'
-import rootReducer from './reducers'
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+import App from './App'
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
